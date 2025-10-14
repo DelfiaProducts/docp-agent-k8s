8a41b7ab7d3a332bd7c6b35b63d7ef151ee02efb
Tom <wellington.gadelha@delfia.tech>
2025-10-14 10:06:41 -0300

    #10[refac: utils/common.go]: change import for pkg

--- utils/common.go
+++ utils/common.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/OryaHub/agent-k8s/internal"
+	pkg "github.com/OryaHub/agent-k8s/pkg"
 )
 
 // GetDomainUrl return domain url
@@ -13,77 +13,77 @@ func GetDomainUrl() (string, error) {
 	if len(oryaDomain) != 0 {
 		return oryaDomain, nil
 	}
-	return internal.ORYA_DOMAIN, nil
+	return pkg.ORYA_DOMAIN, nil
 }
 
 // GetDatadogHelmRepository returns the Datadog Helm repository URL
 func GetDatadogHelmRepository() string {
-	return internal.DATADOG_HELM_REPOSITORY
+	return pkg.DATADOG_HELM_REPOSITORY
 }
 
 // GetDatadogDeploymentOperatorName returns the Datadog deployment operator name
 func GetDatadogDeploymentOperatorName() string {
-	return internal.DATADOG_DEPLOYMENT_OPERATOR_NAME
+	return pkg.DATADOG_DEPLOYMENT_OPERATOR_NAME
 }
 
 // GetDatadogDeploymentClusterAgentName returns the Datadog deployment cluster agent name
 func GetDatadogDeploymentClusterAgentName() string {
-	return internal.DATADOG_DEPLOYMENT_CLUSTER_AGENT_NAME
+	return pkg.DATADOG_DEPLOYMENT_CLUSTER_AGENT_NAME
 }
 
 // GetDatadogDeploymentClusterAgentHelmName returns the Datadog deployment cluster agent Helm name
 func GetDatadogDeploymentClusterAgentHelmName() string {
-	return internal.DATADOG_DEPLOYMENT_CLUSTER_AGENT_HELM_NAME
+	return pkg.DATADOG_DEPLOYMENT_CLUSTER_AGENT_HELM_NAME
 }
 
 // GetHelmRepository returns the Helm repository URL
 func GetHelmRepository() string {
-	return internal.ORYA_HELM_REPOSITORY
+	return pkg.ORYA_HELM_REPOSITORY
 }
 
 // GetHelmRepositoryName returns chart name the orya helm repository
 func GetHelmRepositoryName() string {
-	return internal.ORYA_HELM_REPOSITORY_NAME
+	return pkg.ORYA_HELM_REPOSITORY_NAME
 }
 
 // GetHelmChartName returns the chart name for the orya helm
 func GetHelmChartName() string {
-	return internal.ORYA_HELM_CHART_NAME
+	return pkg.ORYA_HELM_CHART_NAME
 }
 
 // GetOryaNamespace return orya namespace
 func GetOryaNamespace() string {
-	return internal.ORYA_NAMESPACE
+	return pkg.ORYA_NAMESPACE
 }
 
 // GetMutatingWebhookName return orya mutating webhook name
 func GetMutatingWebhookName() string {
-	return internal.ORYA_MUTATING_WEBHOOK_NAME
+	return pkg.ORYA_MUTATING_WEBHOOK_NAME
 }
 
 // GetClusterRoleName return orya cluster role name
 func GetClusterRoleName() string {
-	return internal.ORYA_CLUSTER_ROLE_NAME
+	return pkg.ORYA_CLUSTER_ROLE_NAME
 }
 
 // GetClusterRoleBindingName return orya cluster role binding name
 func GetClusterRoleBindingName() string {
-	return internal.ORYA_CLUSTER_ROLE_BINDING_NAME
+	return pkg.ORYA_CLUSTER_ROLE_BINDING_NAME
 }
 
 // GetOryaConfiMapStateName return orya config map state name
 func GetOryaConfiMapStateName() string {
-	return internal.ORYA_CONFIG_MAP_STATE_NAME
+	return pkg.ORYA_CONFIG_MAP_STATE_NAME
 }
 
 // GetOryaConfigMapConfigurationsName return orya config map configurations name
 func GetOryaConfigMapConfigurationsName() string {
-	return internal.ORYA_CONFIG_MAP_CONFIGURATIONS_NAME
+	return pkg.ORYA_CONFIG_MAP_CONFIGURATIONS_NAME
 }
 
 // GetServiceAccountName return service account name
 func GetServiceAccountName() string {
-	return internal.SERVICE_ACCOUNT_NAME
+	return pkg.SERVICE_ACCOUNT_NAME
 }
 
 // GetOryaReleaseName return release name the orya
@@ -93,7 +93,7 @@ func GetOryaReleaseName() string {
 
 // GetOryaUpdaterRepositoryName return orya updater repository name
 func GetOryaUpdaterRepositoryName(version string) string {
-	name := fmt.Sprintf("%s/%s:%s", internal.ORYA_REPOSITORY_IMAGE_NAME, internal.ORYA_DEPLOYMENT_UPDATER_NAME, version)
+	name := fmt.Sprintf("%s/%s:%s", pkg.ORYA_REPOSITORY_IMAGE_NAME, pkg.ORYA_DEPLOYMENT_UPDATER_NAME, version)
 	return name
 }
 
@@ -105,21 +105,21 @@ func GetRepositoryImage(name string) string {
 		if len(managerImage) > 0 {
 			return managerImage
 		} else {
-			return fmt.Sprintf("%s/%s", internal.ORYA_REPOSITORY_IMAGE_NAME, "k8s-manager")
+			return fmt.Sprintf("%s/%s", pkg.ORYA_REPOSITORY_IMAGE_NAME, "k8s-manager")
 		}
 	case "agent":
 		agentImage := os.Getenv("AGENT_IMAGE_NAME")
 		if len(agentImage) > 0 {
 			return agentImage
 		} else {
-			return fmt.Sprintf("%s/%s", internal.ORYA_REPOSITORY_IMAGE_NAME, "k8s-agent")
+			return fmt.Sprintf("%s/%s", pkg.ORYA_REPOSITORY_IMAGE_NAME, "k8s-agent")
 		}
 	case "webhook":
 		webhookImage := os.Getenv("WEBHOOK_IMAGE_NAME")
 		if len(webhookImage) > 0 {
 			return webhookImage
 		} else {
-			return fmt.Sprintf("%s/%s", internal.ORYA_REPOSITORY_IMAGE_NAME, "k8s-webhook")
+			return fmt.Sprintf("%s/%s", pkg.ORYA_REPOSITORY_IMAGE_NAME, "k8s-webhook")
 		}
 	}
 	return ""
@@ -133,21 +133,21 @@ func GetOryaDeploymentName(name string) string {
 		if len(managerDeployNamme) > 0 {
 			return managerDeployNamme
 		} else {
-			return internal.ORYA_DEPLOYMENT_MANAGER_NAME
+			return pkg.ORYA_DEPLOYMENT_MANAGER_NAME
 		}
 	case "agent":
 		agentDeployName := os.Getenv("AGENT_DEPLOYMENT_NAME")
 		if len(agentDeployName) > 0 {
 			return agentDeployName
 		} else {
-			return internal.ORYA_DEPLOYMENT_AGENT_NAME
+			return pkg.ORYA_DEPLOYMENT_AGENT_NAME
 		}
 	case "webhook":
 		webhookDeployName := os.Getenv("WEBHOOK_DEPLOYMENT_NAME")
 		if len(webhookDeployName) > 0 {
 			return webhookDeployName
 		} else {
-			return internal.ORYA_DEPLOYMENT_WEBHOOK_NAME
+			return pkg.ORYA_DEPLOYMENT_WEBHOOK_NAME
 		}
 	default:
 		return ""
@@ -158,11 +158,11 @@ func GetOryaDeploymentName(name string) string {
 func GetOryaContainerName(name string) string {
 	switch name {
 	case "manager":
-		return internal.CONTAINER_MANAGER_NAME
+		return pkg.CONTAINER_MANAGER_NAME
 	case "agent":
-		return internal.CONTAINER_AGENT_NAME
+		return pkg.CONTAINER_AGENT_NAME
 	case "webhook":
-		return internal.CONTAINER_WEBHOOK_NAME
+		return pkg.CONTAINER_WEBHOOK_NAME
 	default:
 		return ""
 	}
@@ -170,30 +170,30 @@ func GetOryaContainerName(name string) string {
 
 // ErrorOryaApiKeyNotFound return error the api key orya not found
 func ErrorOryaApiKeyNotFound() error {
-	return internal.OryaApiKeyNotFound
+	return pkg.OryaApiKeyNotFound
 }
 
 // ErrAuthTokenClaimsInvalid return error the invalid claims token
 func ErrAuthTokenClaimsInvalid() error {
-	return internal.ErrAuthTokenClaimsInvalid
+	return pkg.ErrAuthTokenClaimsInvalid
 }
 
 // ErrNotFoundChart return error the chart not found
 func ErrNotFoundChart() error {
-	return internal.ErrNotFoundChart
+	return pkg.ErrNotFoundChart
 }
 
 // ErrNotFoundChartVersion return error the chart version not found
 func ErrNotFoundChartVersion() error {
-	return internal.ErrNotFoundChartVersion
+	return pkg.ErrNotFoundChartVersion
 }
 
 // ErrNotFoundReleaseName return error the not found release name
 func ErrNotFoundReleaseName() error {
-	return internal.ErrNotFoundReleaseName
+	return pkg.ErrNotFoundReleaseName
 }
 
 // ErrInvalidDatadogMode return error the invalid datadog mode
 func ErrInvalidDatadogMode() error {
-	return internal.ErrInvalidDatadogMode
+	return pkg.ErrInvalidDatadogMode
 }
