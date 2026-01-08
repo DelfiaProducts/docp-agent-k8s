0410a8eb1bee7166a0c07834568e58258d7e43a9
Tom <wellington.gadelha@delfia.tech>
2026-01-08 09:55:31 -0300

    #16[refac: operators/manager_operator_disptach.go]: add tracer id on notify status logic

--- operators/manager_operator_dispatch.go
+++ operators/manager_operator_dispatch.go
@@ -20,6 +20,13 @@ func (m *ManagerOperator) NotifyStatus(status string, typeEvent string, message 
 		m.logger.Error("notify status get config map", "error", err.Error())
 		return err
 	}
+	//get tracer id from config map state
+	configMapState, err := m.GetConfigMap(utils.GetOryaConfiMapStateName(), factorySignal.Namespace)
+	if err != nil {
+		m.logger.Error("notify status get config map state", "error", err.Error())
+		return err
+	}
+
 	if accessToken, ok := configMapConfiguration.Data["access_token"]; ok {
 		transactionStatus := utils.GetTransactionFromContext(ctx)
 		if len(transactionStatus.ID) > 0 {
@@ -27,6 +34,10 @@ func (m *ManagerOperator) NotifyStatus(status string, typeEvent string, message 
 			transactionStatus.Message = message
 			transactionStatus.TypeEvent = typeEvent
 			transactionStatus.UlidEvent = utils.GetUlid()
+			//inject tracer id if exists
+			if tracerID, ok := configMapState.Data["tracer_id"]; ok && tracerID != "" {
+				transactionStatus.TracerID = tracerID
+			}
 			if m.LockedEvents {
 				m.pendingTransactionEvents = append(m.pendingTransactionEvents, transactionStatus)
 			} else {
